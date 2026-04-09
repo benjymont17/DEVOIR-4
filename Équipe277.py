6af23cf3cf09d669214b9d6d3d2d17fd87e61876
Benjamin Montminy <benjamin.montminy.1@ulaval.ca>
2026-04-09 16:59:02 -0400

    fin question 2a)

--- Équipe277.py
+++ Équipe277.py
@@ -2,6 +2,7 @@ from problimite import problimite
 import numpy as np
 import matplotlib.pyplot as plt
 
+L=6
 k = 1.2
 T_a = 20
 T = 350
@@ -15,8 +16,8 @@ R_2 = [-k**2*T_a]*N_2
 premier = problimite(N_1, Q_1, R_1, 0, 6, T, T_a)
 deuxieme = problimite(N_2, Q_2, R_2, 0, 6, T, T_a)
 
-A = [np.exp(k*0), np.exp(-k*0)], [np.exp(k*6), np.exp(-k*6)]
-b_sol = premier[0:2]
+A = [1, 1], [np.exp(k*L), np.exp(-k*L)]
+b_sol = [T-T_a, 0]
 solution = np.linalg.solve(A, b_sol)
 def T_exacte(x):
      return T_a+solution[0]*np.exp(k*x)+solution[1]*np.exp(-k*x)
@@ -28,11 +29,11 @@ x_2 = np.linspace(0, 6, N_2+2)
 x_exacte = np.linspace(0, 6, 200)
 
 
-plt.plot(x_1, premier, label='h=2)')
-plt.plot(x_2, deuxieme, label='h=1)')
+plt.plot(x_1, premier, label='h=2')
+plt.plot(x_2, deuxieme, label='h=1')
 plt.plot(x_exacte, T_exacte(x_exacte), label='solution exacte')
 plt.xlabel('x')
 plt.ylabel('T(x)')
-plt.title('Solution de l\'équation différentielle avec les valeurs de h=1 et h=2')
+plt.title('Solution de l\'équation différentielle avec les valeurs de h=1, h=2 et la solution exacte')
 plt.legend()
 plt.show()
